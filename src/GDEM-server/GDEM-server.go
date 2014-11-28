15c1a18be991b8c314c10874181d18c33d08dd9c
Yifan Gao <git@gaoyifan.com>
2014-11-28 21:42:53 +0800

    init commit

---
+++ src/GDEM-server/GDEM-server.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"github.com/gorilla/mux"
+	"image"
+	"image/png"
+	"math"
+	"net/http"
+	"os"
+	"runtime"
+	"strconv"
+)
+
+var (
+	prefix   string = "map/ASTGTM2_"
+	suffix   string = "_dem.png"
+	imageMap map[PointInt]image.Image
+)
+
+type Point struct {
+	lon, lat float64
+}
+
+type PointInt struct {
+	lon, lat int
+}
+
+func (c Point) toInt() PointInt {
+	var r PointInt
+	r.lat = (int)(c.lat)
+	r.lon = (int)(c.lon)
+	return r
+}
+func init() {
+	imageMap = make(map[PointInt]image.Image)
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
+
+func XYToLonLat(xtile, ytile int, zoom uint) Point {
+	var lon_deg, lat_deg float64
+	var rt Point
+	b := (1 << zoom)
+	n := float64(b)
+	lon_deg = (float64)(xtile*360)/n - 180
+	lat_deg = math.Atan(math.Sinh(math.Pi*(1-2*(float64)(ytile)/n))) * 180 / math.Pi
+	rt.lon = lon_deg
+	rt.lat = lat_deg
+	return rt
+}
+
+func lnglatToXY(longitude, latitude float64, zoom int) (int, int) {
+	lat_rad := latitude * math.Pi / 180
+	n := 1 << (uint)(zoom)
+	xtile := (int)(math.Floor((longitude + 180) / 360 * (float64)(n)))
+	ytile := (int)((1 - math.Log(math.Tan(lat_rad)+1/math.Cos(lat_rad))/math.Pi) * (float64)(n) / 2)
+	return xtile, ytile
+}
+
+func getImageFileName(p PointInt) string {
+	var (
+		lat_dir, lon_dir string
+	)
+	if p.lat >= 0 {
+		lat_dir = "N"
+	} else {
+		lat_dir = "S"
+	}
+	if p.lon >= 0 {
+		lon_dir = "E"
+	} else {
+		lon_dir = "W"
+	}
+	return fmt.Sprintf("%s%s%d%s%d%s", prefix, lat_dir, p.lat, lon_dir, p.lon, suffix)
+}
+
+func getImage(p Point) image.Image {
+	pInt := p.toInt()
+	img, exist := imageMap[pInt]
+	if !exist {
+		file, err := os.Open(getImageFileName(pInt))
+		if err != nil {
+			return nil
+		}
+		img, err = png.Decode(file)
+		if err != nil {
+			return nil
+		}
+		imageMap[pInt] = img
+	}
+	return img
+}
+
+func mapHandler(w http.ResponseWriter, r *http.Request) {
+	var (
+		i0, j0, size_index, zoomi int
+		zoom                      uint
+		latSpan, lonSpan          float64
+		err                       error
+	)
+	fmt.Println(r.URL)
+	para := mux.Vars(r)
+	if i0, err = strconv.Atoi(para["i"]); err != nil {
+		return
+	}
+	if j0, err = strconv.Atoi(para["j"]); err != nil {
+		return
+	}
+	if zoomi, err = strconv.Atoi(para["zoom"]); err != nil {
+		return
+	}
+	zoom = uint(zoomi)
+	if size_index, err = strconv.Atoi(para["size"]); err != nil {
+		return
+	}
+	pStart := XYToLonLat(i0, j0, zoom)
+	pEnd := XYToLonLat(i0+1, j0+1, zoom)
+	size := 1 << (uint)(size_index)
+	latSpan = (pEnd.lat - pStart.lat) / (float64)(size)
+	lonSpan = (pEnd.lon - pStart.lon) / (float64)(size)
+	for i := 0; i <= size; i++ {
+		var p Point
+		p.lat = pStart.lat + (float64)(i)*latSpan
+		for j := 0; j <= size; j++ {
+			p.lon = pStart.lon + (float64)(j)*lonSpan
+			img := getImage(p)
+			if img == nil {
+				binary.Write(w, binary.LittleEndian, float32(0))
+				continue
+			}
+			x := (int)((p.lat - math.Floor(p.lat)) * (float64)(img.Bounds().Max.X))
+			y := (int)((p.lon - math.Floor(p.lon)) * (float64)(img.Bounds().Max.Y))
+			gray, _, _, _ := img.At(x, y).RGBA()
+			binary.Write(w, binary.LittleEndian, float32(int16(gray)))
+		}
+	}
+}
+
+func main() {
+	route := mux.NewRouter()
+	route.HandleFunc("/{i:[0-9]+}/{j:[0-9]+}/{zoom:[0-9]+}/{size:[0-9]+}/", mapHandler).Methods("GET")
+	http.Handle("/", route)
+	http.ListenAndServe(":8000", nil)
+}
